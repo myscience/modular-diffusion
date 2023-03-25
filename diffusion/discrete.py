003bcb00c25d88ed79e195444c3973b01b23ed1a
myscience <muratore.paolo95@gmail.com>
2023-03-25 17:57:09 +0100

    Added Bit-Diffusion

---
+++ diffusion/discrete.py
@@ -0,0 +1,165 @@
+import torch
+from torch import Tensor
+from torch.nn import Module
+from .diffusion import Diffusion
+
+from .module.utils.misc import default
+
+from einops import reduce
+from einops import rearrange
+from functools import partial
+from typing import Optional
+
+class BitDiffusion(Diffusion):
+    '''
+        Discrete Denoising Diffusion Probabilistic Model as introduced in:
+        "Analog Bits: Generating Discrete Data Using Diffusion Models with
+        Self-Conditioning", Chen et al. (2023) - Hinton's Group.
+    '''
+
+    def __init__(
+        self,
+        model: Module,
+        num_bits : int = 1,
+        bit_scale : float = 1.,
+        sigma_min : float = 0.002,
+        sigma_max : float = 80,
+        ode_solver : str = 'heun_sde',
+        rho_schedule : float = 7,
+        lognorm_mean : float = -1.2,
+        lognorm_std  : float = +1.2,
+        sigma_data : float = 0.5,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            model,
+            ode_solver = ode_solver,
+            **kwargs,
+        )
+
+        self.num_bits = num_bits
+        self.bit_scale = bit_scale
+
+        # Controls for timesteps and schedule generation
+        self.sigma_min = sigma_min
+        self.sigma_max = sigma_max
+        self.rho_schedule = rho_schedule
+
+        # Controls for training
+        self.lognorm_mean = lognorm_mean
+        self.lognorm_std  = lognorm_std
+        self.sigma_data   = sigma_data
+
+        self.log_img_key = f'Bit Diffusion - {ode_solver}'
+
+        self.norm_forward  = partial(self.float2bit, nbits = num_bits, scale = bit_scale)
+        self.norm_backward = partial(self.bit2float, nbits = num_bits)
+
+    # * Functions that define what model actually predicts
+    def c_skip(self, sigma : Optional[Tensor] = None) -> Tensor:
+        return self.sigma_data ** 2 / (sigma ** 2 + self.sigma_data ** 2)
+
+    def c_out(self, sigma : Optional[Tensor] = None) -> Tensor:
+        return sigma * self.sigma_data * (self.sigma_data ** 2 + sigma ** 2) ** -0.5
+    
+    def c_in(self, sigma : Optional[Tensor] = None) -> Tensor:
+        return (sigma ** 2 + self.sigma_data ** 2) ** -0.5
+
+    def c_noise(self, sigma : Optional[Tensor] = None) -> Tensor:
+        return torch.log(sigma.clamp(min = 1e-20)) * 0.25
+
+    # * Functions that define model training
+    def loss_weight(self, sigma : Tensor) -> Tensor:
+        return (sigma ** 2 + self.sigma_data ** 2) * (sigma * self.sigma_data) ** -2
+    
+    def get_noise(self, batch_size : int) -> Tensor:
+        eps = torch.randn((batch_size,), device = self.device)
+        return (self.lognorm_mean + self.lognorm_std * eps).exp()
+    
+    # * Functions that define sampling strategy
+    def get_timesteps(self, num_steps : int, rho : Optional[int] = None) -> Tensor:
+        rho = default(rho, self.rho_schedule)
+
+        inv_rho = 1 / rho
+
+        tramp = torch.linspace(0, 1, num_steps, device = self.device)
+        i_max = self.sigma_max ** inv_rho
+        i_min = self.sigma_min ** inv_rho
+
+        sigma = (i_max + tramp * (i_min - i_max)) ** rho
+
+        return sigma
+
+    def get_schedule(self, t : Tensor, **kwargs) -> Tensor:
+        return t
+
+    def get_scaling(self, t : Tensor, **kwargs) -> Tensor:
+        return torch.ones_like(t)
+
+    @classmethod
+    def int2bit(cls, decs : Tensor, nbits : int = 8, scale : float = 1.) -> Tensor:
+        '''
+            Convert input (int) tensor x (values in [0, 255])
+            to analog bits in [-1, 1].
+        '''
+        device = decs.device
+
+        decs = decs.clamp(min = 0, max = 255).long()
+
+        # Build the bitmask needed for decimal-to-bit conversion
+        mask = 2 ** torch.arange(nbits - 1, -1, -1, device = device, dtype = torch.long)
+
+        mask = rearrange(mask, 'd -> d 1 1').contiguous()
+        decs = rearrange(decs, 'b c h w -> b c 1 h w').contiguous()
+
+        # Get the analog bits
+        bits = ((decs & mask) != 0).float()
+        bits = rearrange(bits, 'b c d h w -> b (c d) h w').contiguous()
+
+        return (bits * 2 - 1) * scale
+    
+    @classmethod
+    def float2bit(cls, decs : Tensor, nbits : int = 8, scale : float = 1.) -> Tensor:
+        '''
+            Convert input (float) tensor x (values in [0, 1])
+            to analog bits in [-1, 1].
+        '''
+        device = decs.device
+
+        decs = (decs * 255).clamp(0, 255).long()
+
+        # Build the bitmask needed for decimal-to-bit conversion
+        mask = 2 ** torch.arange(nbits - 1, -1, -1, device = device, dtype = torch.long)
+
+        mask = rearrange(mask, 'd -> d 1 1').contiguous()
+        decs = rearrange(decs, 'b c h w -> b c 1 h w').contiguous()
+
+        bits = ((decs & mask) != 0).float()
+        bits = rearrange(bits, 'b c d h w -> b (c d) h w').contiguous()
+
+        return (bits * 2 - 1) * scale
+    
+    @classmethod
+    def bit2int(cls, bits : Tensor, nbits : int = 8) -> Tensor:
+        '''
+        '''
+        device = bits.device
+
+        bits = (bits > 0).int()
+        mask = 2 ** torch.arange(nbits - 1, -1, -1, device = device, dtype = int)
+
+        mask = rearrange(mask, 'd -> d 1 1').contiguous()
+        bits = rearrange(bits, 'b (c d) h w -> b c d h w', d = nbits).contiguous()
+
+        decs = reduce(bits * mask, 'b c d h w -> b c h w', 'sum').contiguous()
+
+        return decs.clamp(0, 255)
+
+    @classmethod
+    def bit2float(cls, bits : Tensor, nbits : int = 8) -> Tensor:
+        '''
+        '''
+        
+        decs = cls.bit2int(bits, nbits)
+
+        return (decs / 255).clamp(0., 1.)
--- diffusion/module/unet.py
+++ diffusion/module/unet.py
@@ -17,7 +17,6 @@ from .utils.misc import default
 
 from typing import List, Optional
 
-# ! 35.2M -> 140.862 MB
 class UNet(nn.Module):
     '''
         U-Net model as introduced in:
@@ -32,7 +31,6 @@ class UNet(nn.Module):
         attn_dim : int = 128,
         channels : int = 3,
         ctrl_dim : Optional[int] = None,
-        num_bits : Optional[int] = None,
         chn_mult : List[int] = (1, 2, 4, 8),
         num_group : int = 8,
         num_heads : int = 4,
@@ -41,11 +39,9 @@ class UNet(nn.Module):
     ) -> None:
         super().__init__()
 
+        out_dim = default(out_dim, channels)
 
-        num_bits = default(num_bits, 1)
-        out_dim = default(out_dim, channels * num_bits)
-
-        self.channels = channels * num_bits
+        self.channels = channels
 
         # NOTE: We need channels * 2 to accomodate for the self-conditioning
         self.proj_inp = nn.Conv2d(self.channels * 2, net_dim, 7, padding = 3)
@@ -64,12 +60,6 @@ class UNet(nn.Module):
             nn.GELU(),
             nn.Linear(ctx_dim, ctx_dim)
         )
-
-        # self.fmri_emb = FeatEmbedder(
-        #     patch_len = fmri_patch,
-        #     embed_dim = fmri_dim,
-        #     stride    = fmri_stride,
-        # )
 
         # * Building the model. It has three main components:
         # * 1) The downsampling module
@@ -96,19 +86,16 @@ class UNet(nn.Module):
                 ConvTimeRes(dim_in, dim_in, ctx_dim = ctx_dim, num_group = num_group),
                 ConvTimeRes(dim_in, dim_in, ctx_dim = ctx_dim, num_group = num_group),
                 EfficientAttention(dim_in, attn_dim, qkv_adapt = qkv_adapt, key_dim = ctrl_dim, **attn_kw),
-                # Residual(PreNorm(dim_in, LinearAttention(dim_in))),
                 nn.Conv2d(dim_in, dim_out, 3, padding = 1) if is_last else Downsample(dim_in, dim_out)
             ]))
 
         # Buildup the bottleneck module
         self.mid_block1 = ConvTimeRes(mid_dim, mid_dim, ctx_dim = ctx_dim, num_group = num_group)
         self.mid_attn   = EfficientAttention(mid_dim, attn_dim, qkv_adapt = qkv_adapt, key_dim = ctrl_dim, **attn_kw)
-        # self.mid_attn   = Residual(PreNorm(mid_dim, Attention(mid_dim)))
-        self.mid_block3 = ConvTimeRes(mid_dim, mid_dim, ctx_dim = ctx_dim, num_group = num_group)
+        self.mid_block2 = ConvTimeRes(mid_dim, mid_dim, ctx_dim = ctx_dim, num_group = num_group)
 
         # Build the upsampling module
-        # NOTE: We need to make rooms for incoming residual connections
-        #       from the downsampling layer
+        # NOTE: We need to make rooms for incoming residual connections from the downsampling layers
         for idx, (dim_in, dim_out) in enumerate(reversed(dims)):
             is_last = idx >= (num_resolutions - 1)
 
@@ -116,7 +103,6 @@ class UNet(nn.Module):
                 ConvTimeRes(dim_in + dim_out, dim_out, ctx_dim = ctx_dim, num_group = num_group),
                 ConvTimeRes(dim_in + dim_out, dim_out, ctx_dim = ctx_dim, num_group = num_group),
                 EfficientAttention(dim_out, attn_dim, qkv_adapt = qkv_adapt, key_dim = ctrl_dim, **attn_kw),
-                # Residual(PreNorm(dim_out, LinearAttention(dim_out))),
                 nn.Conv2d(dim_out, dim_in, 3, padding = 1) if is_last else Upsample(dim_out, dim_in)
             ]))
 
@@ -153,8 +139,6 @@ class UNet(nn.Module):
         cond = default(x_c, torch.zeros_like(imgs))
         imgs = torch.cat((cond, imgs), dim = 1)
 
-        # ctrl = self.fmri_emb(fmri) if exists(fmri) else fmri
-
         x = self.proj_inp(imgs)
         t = self.time_emb(time)
 
@@ -166,15 +150,13 @@ class UNet(nn.Module):
 
             x = conv2(x, t)
             x = cross_attn(x, ctrl)
-            # x = cross_attn(x)
             h += [x]
 
             x = down(x)
 
         x = self.mid_block1(x, t)
         x = self.mid_attn(x, ctrl)
-        # x = self.mid_attn(x)
-        x = self.mid_block3(x, t)
+        x = self.mid_block2(x, t)
 
         for conv1, conv2, cross_attn, up in self.ups:
             x = torch.cat((x, h.pop()), dim = 1)
@@ -182,7 +164,6 @@ class UNet(nn.Module):
 
             x = torch.cat((x, h.pop()), dim = 1)
             x = conv2(x, t)
-            # x = cross_attn(x, fmri)
             x = cross_attn(x, ctrl)
 
             x = up(x)
